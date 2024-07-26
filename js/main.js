a7c14bff08e16ffb8d14f3145cedf1d2a25795b6
Traci Serghei <traciserii@gmail.com>
2024-07-26 12:03:00 +0300

    modificari majore in proiect

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-import {locationSvg, phoneSvg, emailSvg, webPageSvg, circleSvg, closeSvg, cameraSvg, moreSvg} from './svg.js'
+import {locationSvg, phoneSvg, emailSvg, webPageSvg, circleSvg, closeSvg, cameraSvg} from './svg.js'
 import { $containerOperators } from './operators.js';
 import { $functionsContainer } from './functions.js';
 import { $loopsContainer } from './loops.js';
 import { $formContainer } from './regForm.js';
 import { $eventsContainer } from './events.js';
+import { $articleWrapper } from './articles.js';
+
 
 const TODAY = new Date();
 const months = ["Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie", "Iulie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"];
@@ -64,43 +66,7 @@ const $title = document.createElement("h1");
 const $registrBtn = document.createElement("button");
 const $titleInfo = document.createElement("h2");
 const $about = document.createElement("p");
-const $workExperienceContainer = document.createElement("div");
-const $workExperienceTitle = document.createElement("h3");
-//primul articol
-const $workExperienceWorkOne = document.createElement("div");
-const $workExperienceWorkOneTitle = document.createElement("h3");
-const $workExperienceWorkOneDescr = document.createElement("p");
-const $workExperienceWorkOneYears = document.createElement("p");
-const $workExperienceWorkOneCircle = document.createElement("span");
-const $workExperienceWorkOneBtn = document.createElement("button");
-//al doilea articol
-const $workExperienceWorkTwo = document.createElement("div");
-const $workExperienceWorkTwoTitle = document.createElement("h3");
-const $workExperienceWorkTwoDescr = document.createElement("p");
-const $workExperienceWorkTwoYears = document.createElement("p");
-const $workExperienceWorkTwoBtn = document.createElement("button");
-const $workExperienceWorkTwoCircle = document.createElement("span");
-//al treilea articol
-const $workExperienceWorkThree = document.createElement("div");
-const $workExperienceWorkThreeTitle = document.createElement("h3");
-const $workExperienceWorkThreeDescr = document.createElement("p");
-const $workExperienceWorkThreeYears = document.createElement("p");
-const $workExperienceWorkThreeCircle = document.createElement("span");
-const $workExperienceWorkThreeBtn = document.createElement("button");
-//al patrulea articol
-const $workExperienceWorkFour = document.createElement("div");
-const $workExperienceWorkFourTitle = document.createElement("h3");
-const $workExperienceWorkFourDescr = document.createElement("p");
-const $workExperienceWorkFourYears = document.createElement("p");
-const $workExperienceWorkFourCircle = document.createElement("span");
-const $workExperienceWorkFourBtn = document.createElement("button");
-//al cincilea articol
-const $workExperienceWorkFive = document.createElement("div");
-const $workExperienceWorkFiveTitle = document.createElement("h3");
-const $workExperienceWorkFiveDescr = document.createElement("p");
-const $workExperienceWorkFiveYears = document.createElement("p");
-const $workExperienceWorkFiveCircle = document.createElement("span");
-const $workExperienceWorkFiveBtn = document.createElement("button");
+const $articlesContainer = document.createElement("div");
 
 //foter
 const $footerTitle = document.createElement("h3");
@@ -169,36 +135,8 @@ $registrBtn.classList.add("btn");
 $title.classList.add("main__content-title");
 $titleInfo.classList.add("main__content-info");
 $about.classList.add("main__content-about");
-$workExperienceContainer.classList.add("experience__container")
-$workExperienceTitle.classList.add("experience__title");
-//clase primul articol
-$workExperienceWorkOne.classList.add("work__experience-box");
-$workExperienceWorkOneTitle.classList.add("work__experience-title");
-$workExperienceWorkOneDescr.classList.add("work__experience-descr");
-$workExperienceWorkOneYears.classList.add("work__experience-years");
-$workExperienceWorkOneCircle.classList.add("work__one-circle");
-$workExperienceWorkOneBtn.classList.add("btn__more");
-//clase al doilea articol
-$workExperienceWorkTwo.classList.add("work__experience-box");
-$workExperienceWorkTwoTitle.classList.add("work__experience-title");
-$workExperienceWorkTwoDescr.classList.add("work__experience-descr");
-$workExperienceWorkTwoYears.classList.add("work__experience-years");
-$workExperienceWorkTwoCircle.classList.add("work__one-circle");
-$workExperienceWorkTwoBtn.classList.add("btn__more");
-//clase al treilea articol
-$workExperienceWorkThree.classList.add("work__experience-box");
-$workExperienceWorkThreeTitle.classList.add("work__experience-title");
-$workExperienceWorkThreeDescr.classList.add("work__experience-descr");
-$workExperienceWorkThreeYears.classList.add("work__experience-years");
-$workExperienceWorkThreeCircle.classList.add("work__one-circle");
-$workExperienceWorkThreeBtn.classList.add("btn__more");
-//clase al patrulea articol
-$workExperienceWorkFour.classList.add("work__experience-box");
-$workExperienceWorkFourTitle.classList.add("work__experience-title");
-$workExperienceWorkFourDescr.classList.add("work__experience-descr");
-$workExperienceWorkFourYears.classList.add("work__experience-years");
-$workExperienceWorkFourCircle.classList.add("work__one-circle");
-$workExperienceWorkFourBtn.classList.add("btn__more");
+$articlesContainer.classList.add("experience__container")
+
 
 //clase footer
 $footerContainer.classList.add("footer__container");
@@ -211,35 +149,6 @@ $title.textContent = "Traci Serghei";
 $registrBtn.textContent = "Inregistreaza-te";
 $titleInfo.textContent = "Front-End Developer";
 $about.textContent = "Un Front-End Developer este persoana care implementează diverse web designuri folosind diverse limbaje de programare. La orice site te-ai uita, aproape tot ce vezi ține de partea de front end development: de la așezarea în pagină, la meniurile prin care navighezi, toate acestea au fost dezvoltate de către un Front-End Developer. Un Front-End Developer va folosi în principal ca limbaje de bază HTML, CSS și JavaScript.";
-
-//primul articol
-$workExperienceTitle.textContent = "Cunostinte in JavaScript";
-$workExperienceWorkOneTitle.textContent = "Operatori in JavaScript";
-$workExperienceWorkOneYears.textContent = "10.06.2024";
-$workExperienceWorkOneDescr.textContent = "Operatorii se folosesc pentru a manipula, combina si modifica datele dintr-un program sau script. Operatori aritmetici, operatori de atribuire, operatori de comparare, operatori logici (booleeni)";
-$workExperienceWorkOneCircle.innerHTML = circleSvg;
-$workExperienceWorkOneBtn.textContent = "Mai mult";
-
-//al doilea articol
-$workExperienceWorkTwoTitle.textContent = "Ce este Funcția în JavaScript?";
-$workExperienceWorkTwoYears.textContent = "12.06.2024";
-$workExperienceWorkTwoDescr.textContent = "Funcțiile sunt foarte importante și utile în orice limbaj de programare, deoarece fac codul reutilizabil. O funcție este un bloc de cod care va fi executat numai dacă este apelat. Dacă aveți câteva linii de cod care trebuie folosite de mai multe ori, puteți crea o funcție care să includă liniile de cod care se repetă și apoi apelați funcția oriunde doriți.";
-$workExperienceWorkTwoCircle.innerHTML = circleSvg;
-$workExperienceWorkTwoBtn.textContent = "Mai mult";
-
-//al treilea articol
-$workExperienceWorkThreeTitle.textContent = "Instructiuni ciclice - repetitive";
-$workExperienceWorkThreeYears.textContent = "14.06.2024";
-$workExperienceWorkThreeDescr.textContent = "Instructiunile repetitive se folosesc atunci cand se doreste efectuarea unei comenzi de mai multe ori.";
-$workExperienceWorkThreeCircle.innerHTML = circleSvg;
-$workExperienceWorkThreeBtn.textContent = "Mai mult";
-
-//al patrulea articol
-$workExperienceWorkFourTitle.textContent = "Evenimente JavaScript";
-$workExperienceWorkFourYears.textContent = "17.06.2024";
-$workExperienceWorkFourDescr.textContent = "Atât evenimentele cât şi tratarea evenimentelor sunt actiuni declanşate de către utilizatori şi pot fi interpretate cu ajutorul functiilor JavaScript. Evenimentele declanşate de către utilizatori, cum sunt: click-stânga de mouse, mişcarea mouse-ului, operatia drag-anddrop sau operatii ale sistemului, cum sunt: încărcarea unui document sau a unei imagini, sunt incidente comune.";
-$workExperienceWorkFourCircle.innerHTML = circleSvg;
-$workExperienceWorkFourBtn.textContent = "Mai mult";
 
 
 //continut left sidebar
@@ -321,60 +230,10 @@ function getTime() {
     $fullTime.textContent = hour + " : " + minutes + " : " +seconds;
 }
 
+//data si ora din sidebar
 getFullDate(TODAY);
 setInterval(getTime, 1000);
 
-//butonul prima tema studiata
-$workExperienceWorkOneBtn.addEventListener("click", ()=> {
-    if($containerOperators.classList.contains("display__none")){
-        $containerOperators.classList.add("display__block");
-        $containerOperators.classList.remove("display__none");
-        $workExperienceWorkOneBtn.textContent = "Ascunde";
-    }else {
-        $containerOperators.classList.add("display__none");
-        $containerOperators.classList.remove("display__block");
-        $workExperienceWorkOneBtn.textContent = "Mai mult";
-    }
-    
-})
-
-//butonul a doua tema studiata
-$workExperienceWorkTwoBtn.addEventListener("click", () => {
-    if($functionsContainer.classList.contains("display__none")){
-        $functionsContainer.classList.add("display__block");
-        $functionsContainer.classList.remove("display__none");
-        $workExperienceWorkTwoBtn.textContent = "Ascunde";
-    }else {
-        $functionsContainer.classList.add("display__none");
-        $functionsContainer.classList.remove("display__block");
-        $workExperienceWorkTwoBtn.textContent = "Mai mult";
-    }
-})
-
-//butonul al treilea tema studiata
-$workExperienceWorkThreeBtn.addEventListener("click", () => {
-    if($loopsContainer.classList.contains("display__none")){
-        $loopsContainer.classList.add("display__block");
-        $loopsContainer.classList.remove("display__none");
-        $workExperienceWorkThreeBtn.textContent = "Ascunde";
-    }else {
-        $loopsContainer.classList.add("display__none");
-        $loopsContainer.classList.remove("display__block");
-        $workExperienceWorkThreeBtn.textContent = "Mai mult";
-    }
-})
-//butonul al patrulea tema studiata
-$workExperienceWorkFourBtn.addEventListener("click", () => {
-    if($eventsContainer.classList.contains("display__none")){
-        $eventsContainer.classList.add("display__block");
-        $eventsContainer.classList.remove("display__none");
-        $workExperienceWorkFourBtn.textContent = "Ascunde";
-    }else {
-        $eventsContainer.classList.add("display__none");
-        $eventsContainer.classList.remove("display__block");
-        $workExperienceWorkFourBtn.textContent = "Mai mult";
-    }
-})
 
 //butonul afiseaza lista studentilor
 $studentsShowList.addEventListener("click", () => {
@@ -398,6 +257,7 @@ $projectFiveLogo.addEventListener("click", () => {
     window.location.assign('https://straciweb.github.io/Rumtibet/');
 })
 
+
 //left sidebar content
 $studentsWrapper.append($studentsTitle, $studentsShowList);
 $dateTimekWrapper.append($fullDate, $fullTime);
@@ -416,18 +276,9 @@ $leftSidebar.append($imgWrapper, $contactsWrapper, $educationWrapper, $studentsW
 
 //main content
 
-//al patrulea articol
-$workExperienceWorkFour.append($workExperienceWorkFourTitle, $workExperienceWorkFourYears,$workExperienceWorkFourDescr, $workExperienceWorkFourCircle, $workExperienceWorkFourBtn, $eventsContainer);
-//al treilea articol
-$workExperienceWorkThree.append($workExperienceWorkThreeTitle, $workExperienceWorkThreeYears,$workExperienceWorkThreeDescr, $workExperienceWorkThreeCircle, $workExperienceWorkThreeBtn, $loopsContainer);
-//al doilea articol
-$workExperienceWorkTwo.append($workExperienceWorkTwoTitle, $workExperienceWorkTwoYears,$workExperienceWorkTwoDescr, $workExperienceWorkTwoCircle, $workExperienceWorkTwoBtn, $functionsContainer);
-//primul articol
-$workExperienceWorkOne.append($workExperienceWorkOneTitle, $workExperienceWorkOneYears,$workExperienceWorkOneDescr, $workExperienceWorkOneCircle, $workExperienceWorkOneBtn, $containerOperators);
-
-$workExperienceContainer.append($workExperienceWorkOne, $workExperienceWorkTwo, $workExperienceWorkThree, $workExperienceWorkFour);
+$articlesContainer.prepend($articleWrapper);
 $mainContentHeader.append($title, $registrBtn, $formContainer);
-$mainContent.append($mainContentHeader, $titleInfo, $about, $workExperienceTitle, $workExperienceContainer);
+$mainContent.append($mainContentHeader, $titleInfo, $about, $articlesContainer);
 $contentContainer.append($leftSidebar, $mainContent);
 $content.append($contentContainer)
 
@@ -438,3 +289,61 @@ $footerContainer.append($footerTitle, $projectsWrapper, $footerInfo);
 $footer.append($footerContainer);
 
 document.body.append($content, $footer);
+
+//extragere butoane
+let buttons = Array.from(document.querySelectorAll('.btn__more')).reverse();
+//extragere articole intreg
+let articles = Array.from(document.querySelectorAll(".article__wrapper")).reverse();
+
+
+//actiune butoane mai mult.
+buttons[3].addEventListener("click", ()=> {
+    articles[3].append($containerOperators);
+    if($containerOperators.classList.contains("display__none")){
+        $containerOperators.classList.add("display__block");
+        $containerOperators.classList.remove("display__none");
+        buttons[3].textContent = "Ascunde";
+    }else {
+        $containerOperators.classList.add("display__none");
+        $containerOperators.classList.remove("display__block");
+        buttons[3].textContent = "Mai mult";
+    }
+    console.log("este");
+})
+buttons[2].addEventListener("click", ()=> {
+    articles[2].append($functionsContainer);
+    if($functionsContainer.classList.contains("display__none")){
+        $functionsContainer.classList.add("display__block");
+        $functionsContainer.classList.remove("display__none");
+        buttons[2].textContent = "Ascunde";
+    }else {
+        $functionsContainer.classList.add("display__none");
+        $functionsContainer.classList.remove("display__block");
+        buttons[2].textContent = "Mai mult";
+    }
+})
+buttons[1].addEventListener("click", () => {
+    articles[1].append($loopsContainer);
+    if($loopsContainer.classList.contains("display__none")){
+        $loopsContainer.classList.add("display__block");
+        $loopsContainer.classList.remove("display__none");
+        buttons[1].textContent = "Ascunde";
+    }else {
+        $loopsContainer.classList.add("display__none");
+        $loopsContainer.classList.remove("display__block");
+        buttons[1].textContent = "Mai mult";
+    }
+})
+
+buttons[0].addEventListener("click", () => {
+    articles[0].append($eventsContainer);
+    if($eventsContainer.classList.contains("display__none")){
+        $eventsContainer.classList.add("display__block");
+        $eventsContainer.classList.remove("display__none");
+        buttons[0].textContent = "Ascunde";
+    }else {
+        $eventsContainer.classList.add("display__none");
+        $eventsContainer.classList.remove("display__block");
+        buttons[0].textContent = "Mai mult";
+    }
+})
